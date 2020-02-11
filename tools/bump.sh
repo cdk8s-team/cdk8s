e25b363c6081a4b4f12eb832d829d3f3b37c8611
Elad Ben-Israel <benisrae@amazon.com>
2020-02-11 17:19:18 +0200

    dry run standard-version

--- tools/bump.sh
+++ tools/bump.sh
@@ -3,5 +3,5 @@ set -euo pipefail
 version=$(node -p "require('./version.json').version")
 npx lerna version ${version} --yes --exact --force-publish=* --no-git-tag-version --no-push
 git add .
-npx standard-version --release-as ${version} --commit-all
+npx standard-version --release-as ${version} --commit-all --dry-run
 
