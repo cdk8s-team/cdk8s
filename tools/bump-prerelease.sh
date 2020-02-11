e999976aa1fba9cd5f794e1863831cd27e921d77
Elad Ben-Israel <benisrae@amazon.com>
2020-02-11 15:08:59 +0200

    release script

--- tools/bump-prerelease.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-commit_id=$(git rev-parse --short HEAD 2>/dev/null || echo "dev")
-exec npx lerna version prerelease --yes --exact --force-publish="*" --no-git-tag-version --no-push --preid "pre+${commit_id}"
--- tools/release.sh
+++ tools/release.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 set -euo pipefail
 
-# publish npm
+#====================================
+# release to github npm repository
+#====================================
+
 echo "//npm.pkg.github.com/:_authToken=${GITHUB_TOKEN}" > ~/.npmrc
+
 cd dist/js
 for tarball in $(ls *.tgz); do
   echo "publishing ${tarball}"
   npm publish --tag pre ${tarball}
 done
-
