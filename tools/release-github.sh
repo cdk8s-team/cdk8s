6309425b0e1b66b191f9c4fad8a4b47a305ec4f4
Elad Ben-Israel <benisrae@amazon.com>
2020-02-12 13:02:18 +0200

    Update release-github.sh

--- tools/release-github.sh
+++ tools/release-github.sh
@@ -2,18 +2,57 @@
 # create a github release
 set -euo pipefail
 
+if ! git diff --exit-code; then
+  echo "error: cannot prepare release since there are unstaged changes in the repo"
+  exit 1
+fi
+
 root=$(cd $(dirname $0)/.. && pwd)
 cd ${root}
 
 repo="awslabs/cdk8s"
-
-# check if we already have the current release
 version="$(node -p "require('./package.json').version")"
+
+# skip if we already have a github release for this version
 echo "checking if we already have a release for ${version}"
 if curl --silent --fail -H "Authorization: token ${GITHUB_TOKEN}" https://api.github.com/repos/${repo}/releases/tags/v${version}; then
   echo "release already exists"
   exit 0
 fi
 
-echo "creating github release..."
-npx standard-version --release-as ${version}
+if cat CHANGELOG.md | grep "## ${version}"; then
+  echo "version already exists in changelog"
+  exit 0
+fi
+
+echo "creating changelog..."
+npx standard-version --skip.bump --skip.commit --skip.tag --release-as ${version}
+
+# extract changelog entry
+git diff CHANGELOG.md | grep "^+[^+]" | sed -e "s/^+//" > /tmp/changelog.txt
+
+# prepare request body
+node <<HERE
+const fs = require('fs');
+const req = {
+  "tag_name": "v${version}",
+  "name": "v${version}",
+  "body": fs.readFileSync('/tmp/changelog.txt', 'utf-8')
+};
+
+fs.writeFileSync('/tmp/req.json', JSON.stringify(req, undefined, 2));
+HERE
+
+echo "creating github release for ${version}"
+curl --request POST \
+  --header "Authorization: token ${GITHUB_TOKEN}" \
+  --header "Content-Type: application/json" \
+  --data @/tmp/req.json \
+  https://api.github.com/repos/${repo}/releases
+
+git add package.json
+git add CHANGELOG.md
+git commit -m 'chore(release): ${version}'
+git tag v${version}
+
+# ready to push
