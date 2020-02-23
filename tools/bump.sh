33260bbce3feb794ae540cf53ea2e837b33d9762
Elad Ben-Israel <benisrae@amazon.com>
2020-02-23 22:52:22 +0200

    chore: default bump to "patch" before we release 1.0.0

--- tools/bump.sh
+++ tools/bump.sh
@@ -3,7 +3,8 @@ set -euo pipefail
 repo_root=$(cd $(dirname $0)/.. && pwd)
 cd ${repo_root}
 
-version="${1:-minor}"
+# pre 1.0.0 we default to "patch" which implies "minor" (i.e. no breaking changes)
+version="${1:-patch}"
 
 yarn install
 npx standard-version --release-as ${version} --skip.tag --bumpFiles package.json
