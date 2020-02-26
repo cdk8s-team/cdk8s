9c5500a0f86c72b4a179c7086f3fb1c4a830f2b3
Elad Ben-Israel <benisrae@amazon.com>
2020-02-26 22:07:51 +0200

    chore: default to minor bump

--- tools/bump.sh
+++ tools/bump.sh
@@ -4,7 +4,7 @@ repo_root=$(cd $(dirname $0)/.. && pwd)
 cd ${repo_root}
 
 # pre 1.0.0 we default to "patch" which implies "minor" (i.e. no breaking changes)
-version="${1:-patch}"
+version="${1:-minor}"
 
 yarn install
 npx standard-version --release-as ${version} --skip.tag --bumpFiles package.json
