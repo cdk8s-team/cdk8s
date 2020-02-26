2de4ce090d0d4b2f9e2d23deefaf4616f8f684eb
Elad Ben-Israel <benisrae@amazon.com>
2020-02-26 14:58:21 +0200

    chore: update bump so it will always perform minor releases

--- tools/bump.sh
+++ tools/bump.sh
@@ -3,8 +3,8 @@ set -euo pipefail
 repo_root=$(cd $(dirname $0)/.. && pwd)
 cd ${repo_root}
 
-# pre 1.0.0 we default to "patch" which implies "minor" (i.e. no breaking changes)
-version="${1:-patch}"
+# default to minor releases
+version="${1:-minor}"
 
 yarn install
 npx standard-version --release-as ${version} --skip.tag --bumpFiles package.json
