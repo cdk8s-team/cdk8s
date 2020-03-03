514f217539c6141ebc57ef680173ac091561ce9f
Elad Ben-Israel <benisrae@amazon.com>
2020-03-03 10:11:29 +0200

    chore: set NPM_CONFIG_REGISTRY
    Since yarn is overriding it and jsii-release-npm logs in through ~/.npmrc, we need to set it manually. I'll fix `jsii-release-npm` to set it explicitly but in the meantime, let's try this.

--- tools/release.sh
+++ tools/release.sh
@@ -6,6 +6,8 @@ scriptdir=$(cd $(dirname $0) && pwd)
 # release to github npm repository
 #====================================
 
+export NPM_CONFIG_REGISTRY="https://registry.npmjs.org/"
+
 npx jsii-release-npm dist/js
 ${scriptdir}/release-github.sh
 
