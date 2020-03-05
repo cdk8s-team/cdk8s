13cfe41f88e705619aec672549508e32d69b5f6a
Elad Ben-Israel <benisrae@amazon.com>
2020-03-05 11:56:29 +0200

    use fs-extra instead of node 12 APIs

--- packages/cdk8s-cli/test/util.ts
+++ packages/cdk8s-cli/test/util.ts
@@ -1,5 +1,5 @@
-import { promises as fs } from 'fs';
-import { spawn } from 'child_process';
+import * as fs from 'fs-extra';
+import { spawn, SpawnOptions } from 'child_process';
 import * as path from 'path';
 import * as os from 'os';
 
@@ -45,14 +45,24 @@ export async function jsiiCompile(workdir: string) {
 
   for (const mod of modules) {
     const sourcedir = path.dirname(require.resolve(`${mod}/package.json`));
-    await fs.mkdir(path.join(workdir, path.join('node_modules', path.dirname(mod))), { recursive: true });
+    await fs.mkdirp(path.join(workdir, path.join('node_modules', path.dirname(mod))));
     await fs.symlink(sourcedir, path.join(workdir, 'node_modules', mod));
   }
 
   await fs.writeFile(path.join(workdir, 'package.json'), JSON.stringify(pkg, undefined, 2));
 
+  await shell(compiler, args, { 
+    cwd: workdir 
+  });
+}
+
+export async function createWorkdir() {
+  return await fs.mkdtemp(path.join(os.tmpdir(), 'generator.tests'));  
+}
+
+export async function shell(command: string, args: string[] = [], options: SpawnOptions = { }) {
   return new Promise((ok, ko) => {
-    const child = spawn(compiler, args, { cwd: workdir, stdio: 'inherit' });
+    const child = spawn(command, args, { stdio: 'inherit', ...options });
     child.once('error', ko);
     child.once('exit', code => {
       if (code === 0) { return ok(); }
@@ -61,6 +71,16 @@ export async function jsiiCompile(workdir: string) {
   });
 }
 
-export async function createWorkdir() {
-  return await fs.mkdtemp(path.join(os.tmpdir(), 'generator.tests'));  
+export async function withTempDir(dirname: string, closure: () => Promise<void>) {
+  const prevdir = process.cwd();
+  const parent = await fs.mkdtemp(path.join(os.tmpdir(), 'cdk8s-init-test.'));
+  const workdir = path.join(parent, dirname);
+  await fs.mkdirp(workdir);
+  try {
+    process.chdir(workdir);
+    await closure();
+  } finally {
+    process.chdir(prevdir);
+    await fs.remove(parent);
+  }
 }
