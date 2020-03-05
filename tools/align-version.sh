caf5ff9b046fe646b3dce3028674644e201340ed
Elad Ben-Israel <benisrae@amazon.com>
2020-03-05 16:21:12 +0200

    chore: release a @next dist-tag for each commit on master
    The @next version number will have the previous version appended with `+<commitish>`.

--- tools/align-version.sh
+++ tools/align-version.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
+#
+# usage: align-version.sh [SUFFIX]
+#
 # aligns lerna version to package.json
 # this is executed in CI builds so artifacts include the actual version instead of 0.0.0.0
+#
+# if SUFFIX is provided, appends this to the version as-is
+#
 
 set -euo pipefail
 scriptdir="$(cd $(dirname $0) && pwd)"
 cd ${scriptdir}/..
 
-version=$(node -p "require('./package.json').version")
+suffix="${1:-}"
+version="$(node -p "require('./package.json').version")${suffix}"
 npx lerna version ${version} --yes --exact --force-publish=* --no-git-tag-version --no-push
 
