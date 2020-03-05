735e840f0ef440e715d34923aed184e58a9f243c
Elad Ben-Israel <benisrae@amazon.com>
2020-03-05 17:13:29 +0200

    feat(cli): typescript - yarn run upgrade & upgrade:next
    add some commands for upgrading your project

--- packages/cdk8s-cli/templates/typescript-app/.hooks.sscaff.js
+++ packages/cdk8s-cli/templates/typescript-app/.hooks.sscaff.js
@@ -2,6 +2,8 @@ const { execSync } = require('child_process');
 const { readFileSync, symlinkSync, mkdirSync } = require('fs');
 const path = require('path');
 
+const version = require('../../package.json').version;
+
 exports.pre = () => {
   if (process.env.CLI_TEST) {
     console.error(`CLI_TEST=1`);
@@ -9,36 +11,38 @@ exports.pre = () => {
 };
 
 exports.post = () => {
-  const deps = [ '@aws-cdk/core', '@aws-cdk/cx-api', 'cdk8s' ];
-  const devDeps = [ 'cdk8s-cli', '@types/node' ];
-
-  deps.forEach(d => installDep(d));
-  devDeps.forEach(d => installDep(d, true));
+  installDeps([ '@aws-cdk/core', '@aws-cdk/cx-api', `cdk8s@^${version}` ]);
+  installDeps([ `cdk8s-cli@^${version}`, '@types/node' ], true);
 
   // build to make sure all is good
   execSync('yarn build', { stdio: 'inherit' });
 
-  console.log(readFileSync('./help.txt', 'utf-8'));
+  console.log(readFileSync('./help', 'utf-8'));
 };
 
-function installDep(dep, isDev) {
-
+function installDeps(deps, isDev) {
   // if we are in a CLI test, install the dependency from the local repo instead
   // of from the public registry.
   if (process.env.CLI_TEST) {
-    installLocalDep(dep);
+    for (const dep of deps) {
+      installLocalDep(dep);
+    }
     return;
   }
 
   const devDep = isDev ? '-D' : '';
-  execSync(`yarn add ${devDep} ${dep}`, { stdio: 'inherit' });
+  execSync(`yarn add ${devDep} ${deps.join(' ')}`, { stdio: 'inherit' });
 }
 
-function installLocalDep(dep) {
+function installLocalDep(depWithVer) {
+  const dep = depWithVer.split('@')[0];
+
   const pkgfile = require.resolve(`${dep}/package.json`);
   const target = path.dirname(pkgfile);
   const source = path.join('node_modules', dep);
   mkdirSync(path.dirname(source), { recursive: true });
+
+  console.error(`symlinking: ${source} => ${target}`);
   symlinkSync(target, source);
 
   const pkg = JSON.parse(readFileSync(pkgfile, 'utf-8'));
@@ -49,6 +53,7 @@ function installLocalDep(dep) {
     const binsource = path.join(localbin, program);
     const bintarget = path.join(target, relpath);
 
+    console.error(`symlinking: ${binsource} => ${binsource}`);
     symlinkSync(bintarget, binsource);
   }
 }
