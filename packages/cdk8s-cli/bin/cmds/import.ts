ed15e525318af8c2a90b532d94265e8ac6e387dc
Elad Ben-Israel <benisrae@amazon.com>
2020-03-06 18:12:58 +0200

    feat(cli): cdk8s.yaml
    Allow specifying certain switches through a local `cdk8s.yaml` file:
    
    - `app` describes the command to run for `cdk8s synth`
    - `language` is the language to use for `cdk8s import`
    - `imports` lists all the api sources to import
    - `output` can be used to specify the synth output directory
    
    Resolves #42

--- packages/cdk8s-cli/bin/cmds/import.ts
+++ packages/cdk8s-cli/bin/cmds/import.ts
@@ -1,42 +1,52 @@
 import * as yargs from 'yargs';
-import { generateAllApiObjects, DEFAULT_API_VERSION } from '../../lib/import';
+import { importKubernetesApi, DEFAULT_API_VERSION } from '../../lib/import';
+import { readConfigSync } from '../../lib/config';
 
-const K8S_SPEC = 'k8s';
+const config = readConfigSync();
+
+const K8S_SOURCE = 'k8s';
 const DEFAULT_OUTDIR = 'imports';
 const LANGUAGES = [ 'typescript', 'python' ];
 
 class Command implements yargs.CommandModule {
-  public readonly command = 'import SPEC';
+  public readonly command = 'import [SOURCE]';
   public readonly describe = 'Imports API objects to your app by generating constructs.';
   public readonly aliases = [ 'gen', 'import', 'generate' ];
 
   public readonly builder = (args: yargs.Argv) => args
-    .positional('SPEC', {  desc: `The API spec to import (only "k8s" is currently supported)` })
-    .demand('SPEC')
+    .positional('SOURCE', { default: config.imports, desc: `The API source to import (supported sources: "${K8S_SOURCE}")`, array: true })
     .example(`cdk8s import k8s`, `Imports Kubenretes API objects to imports/k8s.ts`)
     .example(`cdk8s import k8s@1.13.0`, `imports a specific version of the Kubenretes API`)
 
-    .option('output', { type: 'string', desc: 'Output directory', default: DEFAULT_OUTDIR, alias: 'o' })
+    .option('output', { default: DEFAULT_OUTDIR, type: 'string', desc: 'Output directory', alias: 'o' })
     .option('include', { type: 'array', desc: 'Types to select instead of the default which is latest stable version' })
     .option('exclude', { type: 'array', desc: 'Do not import types that match these regular expressions. They will be represented as the "any" type' })
-    .option('language', { demand: true, type: 'string', desc: 'Output programming language', alias: 'l', choices: LANGUAGES });
+    .option('language', { default: config.language, demand: true, type: 'string', desc: 'Output programming language', alias: 'l', choices: LANGUAGES });
 
   public async handler(argv: any) {
-    const spec = argv.spec;
-    const [ src, specver ] = spec.split('@');
+    const sources = Array.isArray(argv.source) ? argv.source : [ argv.source ];
 
-    const apiVersion = specver ?? DEFAULT_API_VERSION;
-
-    if (src !== K8S_SPEC) {
-      throw new Error(`currently we only support the source ${K8S_SPEC}`);
+    if (sources.length > 1) {
+      throw new Error(`only a single source is supported at the moment. got: ${sources.join(',')}`);
     }
 
-    await generateAllApiObjects(argv.output, {
-      apiVersion,
-      include: argv.include,
-      exclude: argv.exclude,
-      language: argv.language
-    });
+    for (const source of sources) {
+      const [ src, srcver ] = source.split('@');
+
+      const apiVersion = srcver ?? DEFAULT_API_VERSION;
+  
+      if (src !== K8S_SOURCE) {
+        throw new Error(`unsupported import source "${src}", supported sources: "${K8S_SOURCE}"`);
+      }
+  
+      await importKubernetesApi(argv.output, {
+        apiVersion,
+        include: argv.include,
+        exclude: argv.exclude,
+        language: argv.language
+      });
+    }
+
   }
 }
 
