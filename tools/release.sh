6c7bb35a83f3c6db4979386f3feac2039032df3f
campionfellin <campionfellin@gmail.com>
2020-03-15 22:41:36 -0700

    chore: release to all package managers in parallel
    Signed-off-by: campionfellin <campionfellin@gmail.com>

--- tools/release.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-scriptdir=$(cd $(dirname $0) && pwd)
-
-# release to all package managers
-npx jsii-release
- 
-# release to github releases (still not supported by jsii-release)
-${scriptdir}/release-github.sh
