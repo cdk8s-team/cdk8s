cc75ba0ee7335a590bdca021913534124deaea47
campionfellin <campionfellin@gmail.com>
2020-03-19 21:20:26 -0700

    fix(crd): fix multi-resource importing of CRDs
    https://github.com/awslabs/cdk8s/issues/77
    
    Signed-off-by: campionfellin <campionfellin@gmail.com>

--- packages/cdk8s-cli/test/import/import-crd.test.ts
+++ packages/cdk8s-cli/test/import/import-crd.test.ts
@@ -4,22 +4,59 @@ import * as fs from 'fs';
 import { ImportCustomResourceDefinition } from '../../lib/import/crd';
 import { expectImportMatchSnapshot } from './util';
 
-const jenkinsCrd = yaml.parse(fs.readFileSync(path.join(__dirname, 'fixtures', 'jenkins_crd.yaml'), 'utf-8'));
+const jenkinsCrd = yaml.parseAllDocuments(fs.readFileSync(path.join(__dirname, 'fixtures', 'jenkins_crd.yaml'), 'utf-8'))
+                       .map((doc: yaml.ast.Document) => doc.toJSON());
 
 expectImportMatchSnapshot('jenkins', () => new ImportCustomResourceDefinition(jenkinsCrd));
 
-test('fails if manifest is not a CRD', async () => {
-  expect(() => new ImportCustomResourceDefinition({ 
-    apiVersion: 'voo' 
-  })).toThrow('invalid CustomResourceDefinition manifest: "apiVersion" must be "apiextensions.k8s.io/v1beta1"');
 
-  expect(() => new ImportCustomResourceDefinition({ 
+const multiObjectCRD = yaml.parseAllDocuments(fs.readFileSync(path.join(__dirname, 'fixtures', 'multi_object_crd.yaml'), 'utf-8'))
+                           .map((doc: yaml.ast.Document) => doc.toJSON());
+
+expectImportMatchSnapshot('multiObject', () => new ImportCustomResourceDefinition(multiObjectCRD));
+
+test('fails if manifest is not a CRD', async () => {
+  expect(() => new ImportCustomResourceDefinition([{ 
+    apiVersion: 'voo' 
+  }])).toThrow('invalid CustomResourceDefinition manifest: "apiVersion" must be "apiextensions.k8s.io/v1beta1"');
+
+  expect(() => new ImportCustomResourceDefinition([{ 
     apiVersion: 'apiextensions.k8s.io/v1beta1',
     kind: 'Foo'
-  })).toThrow('invalid CustomResourceDefinition manifest: "kind" must be "CustomResourceDefinition"');
+  }])).toThrow('invalid CustomResourceDefinition manifest: "kind" must be "CustomResourceDefinition"');
 
-  expect(() => new ImportCustomResourceDefinition({
+  expect(() => new ImportCustomResourceDefinition([{
     apiVersion: 'apiextensions.k8s.io/v1beta1',
     kind: 'CustomResourceDefinition',
-  })).toThrow('manifest does not have a "spec" attribute');
+  }])).toThrow('manifest does not have a "spec" attribute');
+})
+
+test('fails if one apiObject in multiObject CRD is not a valid CRD', async () => {
+  expect(() => new ImportCustomResourceDefinition([
+    { 
+      apiVersion: 'apiextensions.k8s.io/v1beta1',
+      kind: 'CustomResourceDefinition',
+      metadata: {
+        name: 'testMetadata'
+      },
+      spec: {
+        group: 'testGroup',
+        names: {
+          kind: 'testNameKind'
+        },
+        versions: [{
+          name: 'testVersionName',
+          schema: {
+            openAPIV3Schema: {
+              type: 'testObject'
+            }
+          }
+        }]
+      }
+    },
+    {
+      apiVersion: 'apiextensions.k8s.io/v1beta1',
+      kind: 'CustomResourceDefinition'
+    }
+  ])).toThrow('manifest does not have a "spec" attribute');
 })
