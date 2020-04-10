43405a4dd3fbfa095f59b731b12b533f2e2ffc00
campionfellin <campionfellin@gmail.com>
2020-04-10 13:27:29 -0700

    Update integ tests
    Signed-off-by: campionfellin <campionfellin@gmail.com>

--- test/test-imports/python/test.sh
+++ test/test-imports/python/test.sh
@@ -8,12 +8,12 @@ mkdir test && cd test
 cp ${scriptdir}/cdk8s.yaml  .
 cp ${scriptdir}/mattermost_crd.yaml .
 
-cdk8s import mattermost:=mattermost_crd.yaml --language python || echo "Named import not supported yet."
+cdk8s import mattermost:=mattermost_crd.yaml --language python 2> /dev/null || echo "Named import not supported yet."
 
 # Using the cdk8s.yaml config, with named import
 # Should fail to import named import but successful
 # for others
-cdk8s import --language python || echo "Named import not supported yet."
+cdk8s import --language python 2> /dev/null || echo "Named import not supported yet."
 diff -r imports ${scriptdir}/expected-from-config
 diff -r imports ${scriptdir}/expected-from-config
 
