f783221f7c47592a231ddf25a3cfb32e1a2a0da9
campionfellin <campionfellin@gmail.com>
2020-05-13 21:20:51 -0700

    feat(examples): Examples for hello-java and web-service-java
    Signed-off-by: campionfellin <campionfellin@gmail.com>

---
+++ examples/web-service-java/src/main/java/com/mycompany/app/HelloKube.java
@@ -0,0 +1,42 @@
+package com.mycompany.app;
+
+import software.constructs.Construct;
+
+import org.cdk8s.App;
+import org.cdk8s.Chart;
+import org.cdk8s.ChartOptions;
+
+/**
+ * Hello world!
+ */
+public class HelloKube extends Chart 
+{
+
+    public HelloKube(final Construct scope, final String id) {
+        this(scope, id, null);
+    }
+
+    public HelloKube(final Construct scope, final String id, final ChartOptions options) {
+        super(scope, id, options);
+
+        final WebServiceOptions helloOptions = new WebServiceOptions.Builder()
+            .image("paulbouwer/hello-kubernetes:1.7")
+            .replicas(2)
+            .build();
+        
+        new WebService(this, "hello", helloOptions);
+
+        final WebServiceOptions ghostOptions = new WebServiceOptions.Builder()
+            .image("ghost")
+            .containerPort(2368)
+            .build();
+        
+        new WebService(this, "ghost", ghostOptions);
+    }
+
+    public static void main(String[] args) {
+        final App app = new App();
+        new HelloKube(app, "web-service-java");
+        app.synth();
+    }
+}
