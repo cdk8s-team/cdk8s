9cc9bb97bafe119634674afd517936b66a73b62b
Matthew Bonig <matthew.bonig@gmail.com>
2020-05-18 08:30:17 -0600

    using Testing.app instead, and toMatchSnapshot()

--- test/test-typescript-app/main.test.ts
+++ test/test-typescript-app/main.test.ts
@@ -1,9 +1,9 @@
 import {MyChart} from './main';
-import {App, Testing} from "cdk8s";
+import {Testing} from "cdk8s";
 
 describe('Simple', () => {
     test('one thing', () => {
-        const app = new App();
+        const app = Testing.app();
         const chart = new MyChart(app, 'test-chart');
         const results = Testing.synth(chart)
         expect(results).toHaveLength(1);
