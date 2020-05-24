9e0f030ecdb249d9095d0d774b33919a17cc48f7
Elad Ben-Israel <benisrae@amazon.com>
2020-05-24 16:25:42 +0300

    feat(lib): yaml utility functions (#198)
    Introduce `Yaml.save(file, docs)`, `Yaml.tmp(docs)` and `Yaml.load(urlOrFile)` to standardize all YAML save/load operations.
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

---
+++ packages/cdk8s/lib/_loadurl.ts
@@ -0,0 +1,50 @@
+// this is an executable program that downloads a file from a url (with
+// redirects) and prints it's output to STDOUT. it is implemented as a program
+// because node.js does not support issuing network requests synchronously and
+// in the cdk we need everything to be synchronous (it all happens in the
+// ctors). so we utilize `spawnSync` to spawn this program as a child process.
+// alternatively we could have use `curl` but this is more portable.
+
+import { http, https } from 'follow-redirects';
+import { parse } from 'url';
+import * as fs from 'fs';
+
+const url = process.argv[2];
+if (!url) {
+  console.error(`Usage: ${process.argv[1]} URL`);
+  process.exit(1);
+}
+
+const purl = parse(url);
+
+if (!purl.protocol) {
+  if (!purl.pathname) {
+    throw new Error(`unable to parse pathname from file url: ${url}`);
+  }
+
+  process.stdout.write(fs.readFileSync(purl.pathname, 'utf-8'));
+  process.exit(0);
+}
+
+const client = getHttpClient();
+
+const get = client.get(url, response => {
+  if (response.statusCode !== 200) {
+    throw new Error(`${response.statusCode} response from http get: ${response.statusMessage}`);
+  }
+
+  response.on('data', chunk => process.stdout.write(chunk));
+});
+
+get.once('error', err => {
+  throw new Error(`http error: ${err.message}`);
+});
+
+function getHttpClient() {
+  switch (purl.protocol) {
+    case 'http:': return http;
+    case 'https:': return https;
+    default:
+      throw new Error(`unsupported protocol "${purl.protocol}" in url: ${url}`);
+  }
+}
