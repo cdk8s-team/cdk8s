05b09f1acd87791d8c455d1572105ed88d25bf3c
campionfellin <campionfellin@gmail.com>
2020-06-02 21:57:46 -0700

    chore: update pipfile for python-app template
    Signed-off-by: campionfellin <campionfellin@gmail.com>

--- test/test-imports/python/expected-from-cli/mattermost/com/clusterinstallation/_jsii/__init__.py
+++ test/test-imports/python/expected-from-cli/mattermost/com/clusterinstallation/_jsii/__init__.py
@@ -8,8 +8,8 @@ import jsii
 import jsii.compat
 import publication
 
-import cdk8s
-import constructs
+import cdk8s._jsii
+import constructs._jsii
 
 __jsii_assembly__ = jsii.JSIIAssembly.load("generated", "0.0.0", __name__[0:-6], "generated@0.0.0.jsii.tgz")
 
