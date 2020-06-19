925a45bb2045b435798c6735b9b9fd1a582f0c38
Elad Ben-Israel <benisrae@amazon.com>
2020-06-19 16:51:43 +0300

    initial ServiceAccount tests

--- packages/cdk8s-plus/src/service-account.ts
+++ packages/cdk8s-plus/src/service-account.ts
@@ -2,6 +2,8 @@ import { Resource, ResourceProps, IResource } from './base';
 import { Construct } from 'constructs';
 import * as k8s from './imports/k8s';
 import { ApiObject } from 'cdk8s';
+import { ISecret } from './secret';
+import { lazy, undefinedIfEmpty } from './utils';
 
 export interface ServiceAccountProps extends ResourceProps {
 
@@ -11,20 +13,62 @@ export interface IServiceAccount extends IResource {
 
 }
 
+export interface ServiceAccountProps {
+  /**
+   * List of secrets allowed to be used by pods running using this
+   * ServiceAccount.
+   *
+   * @see https://kubernetes.io/docs/concepts/configuration/secret
+   */
+  readonly secrets?: ISecret[];
+}
+
+/**
+ * A service account provides an identity for processes that run in a Pod.
+ *
+ * When you (a human) access the cluster (for example, using kubectl), you are
+ * authenticated by the apiserver as a particular User Account (currently this
+ * is usually admin, unless your cluster administrator has customized your
+ * cluster). Processes in containers inside pods can also contact the apiserver.
+ * When they do, they are authenticated as a particular Service Account (for
+ * example, default).
+ *
+ * @see https://kubernetes.io/docs/tasks/configure-pod-container/configure-service-account
+ */
 export class ServiceAccount extends Resource implements IServiceAccount {
-  public static fromServiceAccountName(serviceAccountName: string): IServiceAccount {
-    return {
-      name: serviceAccountName,
-    };
+
+  /**
+   * Imports a service account from the cluster as a reference.
+   * @param name The name of the service account resource.
+   */
+  public static fromServiceAccountName(name: string): IServiceAccount {
+    return { name: name }
   }
 
   public readonly apiObject: ApiObject;
 
-  constructor(scope: Construct, id: string, props: ServiceAccountProps) {
+  /**
+   * List of secrets allowed to be used by pods running using this
+   * ServiceAccount.
+   */
+  public readonly secrets: ISecret[];
+
+  constructor(scope: Construct, id: string, props: ServiceAccountProps = { }) {
     super(scope, id, props);
+
+    this.secrets = props.secrets ?? [];
 
     this.apiObject = new k8s.ServiceAccount(this, 'Resource', {
       metadata: this.metadata._toKube(),
+      secrets: lazy(() => undefinedIfEmpty(this.secrets.map(s => ({ name: s.name })))),
     });
+  }
+
+  /**
+   * Allow a secret to be accessed by pods using this service account.
+   * @param secret The secret
+   */
+  public addSecret(secret: ISecret) {
+    this.secrets.push(secret);
   }
 }
---
+++ packages/cdk8s-plus/test/service-account.test.ts
@@ -0,0 +1,45 @@
+import * as kplus from '../src';
+import { Testing } from 'cdk8s';
+import { Secret } from '../src';
+
+test('minimal definition', () => {
+  // GIVEN
+  const chart = Testing.chart();
+
+  // WHEN
+  new kplus.ServiceAccount(chart, 'my-service-account');
+
+  // THEN
+  expect(Testing.synth(chart)).toMatchInlineSnapshot(`
+    Array [
+      Object {
+        "apiVersion": "v1",
+        "kind": "ServiceAccount",
+        "metadata": Object {
+          "annotations": Object {},
+          "labels": Object {},
+          "name": "test-my-service-account-resource-a5be5a3b",
+        },
+      },
+    ]
+  `);
+});
+
+test('secrets can be added to the service account', () => {
+  // GIVEN
+  const chart = Testing.chart();
+  const secret1 = Secret.fromSecretName('my-secret-1');
+  const secret2 = Secret.fromSecretName('my-secret-2');
+
+  // WHEN
+  const sa = new kplus.ServiceAccount(chart, 'my-service-account', {
+    secrets: [secret1],
+  });
+
+  sa.addSecret(secret2);
+
+  // THEN
+  const manifest = Testing.synth(chart)
+  expect(manifest[0]?.secrets[0]).toStrictEqual({ name: 'my-secret-1' });
+  expect(manifest[0]?.secrets[1]).toStrictEqual({ name: 'my-secret-2' });
+});
