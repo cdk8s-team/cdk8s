2bc5ece6c9941f35bc169e79e267a622ff095b10
campionfellin <campionfellin@gmail.com>
2020-06-23 21:34:23 -0700

    fix: self review
    Signed-off-by: campionfellin <campionfellin@gmail.com>

--- packages/cdk8s-cli/templates/java-app/.hooks.sscaff.js
+++ packages/cdk8s-cli/templates/java-app/.hooks.sscaff.js
@@ -7,18 +7,18 @@ const jsii_runtime_version = '1.5.0'
 
 exports.pre = (variables) => {
   try {
-    execSync('which mvn')
-    let version = variables.mvn_cdk8s
-    if (variables.mvn_cdk8s.endsWith('.jar')) {
-      version = variables.mvn_cdk8s.split('-')[1].split('.jar')[0]
-    }
-    variables.CDK8S_VERSION = version
-    variables.JSII_RUNTIME_VERSION = jsii_runtime_version
-    variables.CONSTRUCTS_VERSION = constructs_version.replace('^', '').replace('~', '')
+    execSync('which mvn');
   } catch {
-    console.error(`Unable to find "mvn". Install from https://maven.apache.org/install.html`)
+    console.error(`Unable to find "mvn". Install from https://maven.apache.org/install.html`);
     process.exit(1);
   }
+  let version = variables.mvn_cdk8s;
+  if (variables.mvn_cdk8s.endsWith('.jar')) {
+    version = variables.mvn_cdk8s.split('-')[1].split('.jar')[0]
+  }
+  variables.CDK8S_VERSION = version;
+  variables.JSII_RUNTIME_VERSION = jsii_runtime_version;
+  variables.CONSTRUCTS_VERSION = constructs_version.replace('^', '').replace('~', '');
 };
 
 exports.post = options => {
@@ -27,7 +27,7 @@ exports.post = options => {
     throw new Error(`missing context "mvn_cdk8s"`);
   }
 
-  execSync(`mvn install`)
+  execSync(`mvn install`);
   execSync(`${cli} import k8s -l java`);
   execSync(`mvn compile`);
   execSync(`mvn exec:java -Dexec.mainClass="com.mycompany.app.Main"`);
