78c4136b9c090fde5661a37f043a8666afd55532
campionfellin <campionfellin@gmail.com>
2020-06-23 21:31:41 -0700

    fix: use sscaff to replace dependency versions
    Signed-off-by: campionfellin <campionfellin@gmail.com>

--- packages/cdk8s-cli/templates/java-app/.hooks.sscaff.js
+++ packages/cdk8s-cli/templates/java-app/.hooks.sscaff.js
@@ -2,10 +2,19 @@ const { execSync } = require('child_process');
 const { readFileSync } = require('fs');
 
 const cli = require.resolve('../../bin/cdk8s');
+const constructs_version = require('../../package.json').dependencies.constructs;
+const jsii_runtime_version = '1.5.0'
 
-exports.pre = () => {
+exports.pre = (variables) => {
   try {
     execSync('which mvn')
+    let version = variables.mvn_cdk8s
+    if (variables.mvn_cdk8s.endsWith('.jar')) {
+      version = variables.mvn_cdk8s.split('-')[1].split('.jar')[0]
+    }
+    variables.CDK8S_VERSION = version
+    variables.JSII_RUNTIME_VERSION = jsii_runtime_version
+    variables.CONSTRUCTS_VERSION = constructs_version.replace('^', '').replace('~', '')
   } catch {
     console.error(`Unable to find "mvn". Install from https://maven.apache.org/install.html`)
     process.exit(1);
@@ -18,14 +27,7 @@ exports.post = options => {
     throw new Error(`missing context "mvn_cdk8s"`);
   }
 
-  if (mvn_cdk8s.endsWith('.jar')) {
-    const localVersion = mvn_cdk8s.split('-')[1].split('.jar')[0]
-    execSync(`mvn install:install-file -Dfile=${mvn_cdk8s} -DgroupId=org.cdk8s -DartifactId=cdk8s -Dversion=${localVersion} -Dpackaging=jar`)
-  } else {
-    execSync(`mvn versions:use-dep-version -Dincludes=org.cdk8s:cdk8s -DdepVersion=${mvn_cdk8s} -DforceVersion=true`)
-    execSync('rm pom.xml.versionsBackup')
-  }
-
+  execSync(`mvn install`)
   execSync(`${cli} import k8s -l java`);
   execSync(`mvn compile`);
   execSync(`mvn exec:java -Dexec.mainClass="com.mycompany.app.Main"`);
