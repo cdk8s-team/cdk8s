90c1bc2bbf889965b4c30ef7d0b47a02d6dac00e
Eli Polonsky <epolon@amazon.com>
2020-08-24 23:39:15 +0300

    mid work

--- packages/cdk8s-plus/test/eks.test.ts
+++ packages/cdk8s-plus/test/eks.test.ts
@@ -1,7 +1,8 @@
 import * as kplus from '../src';
+import * as k8s from '../src/imports/k8s';
 import * as cdk from '@aws-cdk/core';
 import * as eks from '@aws-cdk/aws-eks';
-import { Testing } from 'cdk8s';
+import * as cdk8s from 'cdk8s';
 import '@aws-cdk/assert/jest';
 
 test('ignores aws-cdk tokens during synth', () => {
@@ -13,7 +14,27 @@ test('ignores aws-cdk tokens during synth', () => {
     version: eks.KubernetesVersion.V1_17,
   })
 
-  const kubeChart = Testing.chart();
+  const kubeApp = new cdk8s.App();
+  const kubeChart = new cdk8s.Chart(kubeApp, 'Chart');
+
+  new k8s.ConfigMap(kubeChart, 'ConfigMap', {
+    data: {
+      brokerUrl: kafka.brokerUrl,
+    },
+  })
+
+  eksCluster.addManifest('config', kubeChart.toJson());
+
+  eksCluster.addManifest('kafka-config', {
+    kind: 'ConfigMap',
+    apiVersion: 'v1',
+    metadata: {
+      name: 'kafka-config',
+    },
+    data: {
+      brokerUrl: query.getResponseField('BootstrapBrokerStringTls'),
+    },
+  })
   const configMap = new kplus.ConfigMap(kubeChart, 'ConfigMap');
   configMap.addData('clusterArn', eksCluster.clusterArn)
 
