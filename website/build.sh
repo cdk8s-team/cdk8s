770ddabe5f71a0e3f9f82e20dc32188e8fb5e735
Elad Ben-Israel <benisrae@amazon.com>
2020-08-26 20:37:53 +0300

    fix(website): doc links are broken due to wrong version number
    The version number was originally read from `package.json` and at some point was changed to be in `lerna.json`.
    
    Fixes #307

--- website/build.sh
+++ website/build.sh
@@ -3,6 +3,6 @@ set -euo pipefail
 scriptdir="$(cd $(dirname $0) && pwd)"
 
 # used to render the references to reference documentation
-export CDK8S_VERSION=$(node -p "require('../package.json').version")
+export CDK8S_VERSION=$(node -p "require('../lerna.json').version")
 
 hugo --minify $@
