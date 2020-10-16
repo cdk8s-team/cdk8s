b9b9abf394715166370324eb6324b0fe5d0a06ef
Neil Kuan <guan840912@gmail.com>
2020-10-16 09:47:08 +0800

    feat(examples): add aws-alb-ingress-controller typescript example
    update main.test.ts & __snapshot__

---
+++ examples/typescript/aws-alb-ingress-controller/main.test.ts
@@ -0,0 +1,11 @@
+import { MyChart } from './main';
+import { Testing } from 'cdk8s';
+
+describe('Placeholder', () => {
+  test('Empty', () => {
+    const app = Testing.app();
+    const chart = new MyChart(app, 'test-chart');
+    const results = Testing.synth(chart)
+    expect(results).toMatchSnapshot();
+  });
+});
