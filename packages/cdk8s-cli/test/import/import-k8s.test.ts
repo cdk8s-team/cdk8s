a107a9f7db248e1712a44c07661c8eee5587b0bd
Phil Prasek <prasek@gmail.com>
2020-11-16 02:33:41 -0800

    feat(cli): import from crds.dev (#378)
    Introduce crds.dev import shortcut: `cdk8s import github:account/repo[@VERSION]`
    
    Example:
    `cdk8s import github:crossplane/crossplane@0.14.0`
    
    Resolves #377
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*
    
    Signed-off-by: Phil Prasek <prasek@gmail.com>

--- packages/cdk8s-cli/test/import/import-k8s.test.ts
+++ packages/cdk8s-cli/test/import/import-k8s.test.ts
@@ -1,8 +1,8 @@
 import { ImportKubernetesApi } from '../../src/import/k8s';
-import { expectImportMatchSnapshot } from './util';
+import { testImportMatchSnapshot } from './util';
 
 const k8s = (v: string) =>
-  expectImportMatchSnapshot(`k8s@${v}`, () => new ImportKubernetesApi({ apiVersion: v }));
+  testImportMatchSnapshot(`k8s@${v}`, () => new ImportKubernetesApi({ apiVersion: v }));
 
 k8s('1.14.0');
 k8s('1.15.0');
