dcdb4e7496ef1da95562c6ac43c6cabf5d76b52b
Eli Polonsky <Eli.polonsky@gmail.com>
2020-11-17 17:31:35 +0200

    chore: host kubernetes spec in our repo (#382)
    We are currently relying on the [instrumenta/kubernetes-json-schema](https://github.com/instrumenta/kubernetes-json-schema) repo in our cli when running `cdk8s import k8s`.
    
    That repo doesn't currently have the `1.19.0` spec, and hasn't seen any commits since April.
    
    Fortunately, the spec generation is easy because of [instrumenta/openapi2jsonschema](https://github.com/instrumenta/openapi2jsonschema) - so we will now generate the specs ourselves and host it in this repo.
    
    Once this PR is merged, we can change the CLI code to use it.
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

---
+++ tools/import-spec.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#---------------------------------------------------------------------------------------------------------------------
+# Import the kubernetes OpenApi specs from https://github.com/kubernetes/kubernetes/tree/master/api/openapi-spec
+# and translates them to JSON Schema. These specs are later used by the
+# 'cdk8s import k8s' command.
+#
+# See https://github.com/instrumenta/openapi2jsonschema
+#---------------------------------------------------------------------------------------------------------------------
+set -euo pipefail
+scriptdir=$(cd $(dirname $0) && pwd)
+rootdir=${scriptdir}/..
+
+version=${1:-}
+
+if [ -z ${version} ]; then
+  echo "Error: Version is required"
+  echo "Usage: ${scriptdir}/import-spec.sh VERSION"
+  exit 1
+fi
+
+schemas_dir=${rootdir}/kubernetes-schemas
+
+mkdir -p ${schemas_dir}
+
+pip install openapi2jsonschema
+
+cd ${schemas_dir}
+
+schema_dir=v${version}
+mkdir -p ${schema_dir}
+cd ${schema_dir}
+
+openapi2jsonschema --kubernetes https://raw.githubusercontent.com/kubernetes/kubernetes/v${version}/api/openapi-spec/swagger.json -o .
+ls | grep -v _definitions.json | xargs rm
