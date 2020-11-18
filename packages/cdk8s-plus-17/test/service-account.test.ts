413f9e5ddd1ded36f26cdf1fea05ac7d67467885
Elad Ben-Israel <benisrae@amazon.com>
2020-11-18 23:11:37 +0200

    update tests

--- packages/cdk8s-plus-17/test/service-account.test.ts
+++ packages/cdk8s-plus-17/test/service-account.test.ts
@@ -1,23 +1,23 @@
-import { Testing, ApiObject } from "cdk8s";
-import { Node } from "constructs";
-import * as kplus from "../src";
+import { Testing, ApiObject } from 'cdk8s';
+import { Node } from 'constructs';
+import * as kplus from '../src';
 
-test("defaultChild", () => {
+test('defaultChild', () => {
   const chart = Testing.chart();
 
   const defaultChild = Node.of(
-    new kplus.ServiceAccount(chart, "ServiceAccount")
+    new kplus.ServiceAccount(chart, 'ServiceAccount'),
   ).defaultChild as ApiObject;
 
-  expect(defaultChild.kind).toEqual("ServiceAccount");
+  expect(defaultChild.kind).toEqual('ServiceAccount');
 });
 
-test("minimal definition", () => {
+test('minimal definition', () => {
   // GIVEN
   const chart = Testing.chart();
 
   // WHEN
-  new kplus.ServiceAccount(chart, "my-service-account");
+  new kplus.ServiceAccount(chart, 'my-service-account');
 
   // THEN
   expect(Testing.synth(chart)).toMatchInlineSnapshot(`
@@ -33,21 +33,21 @@ test("minimal definition", () => {
   `);
 });
 
-test("secrets can be added to the service account", () => {
+test('secrets can be added to the service account', () => {
   // GIVEN
   const chart = Testing.chart();
-  const secret1 = kplus.Secret.fromSecretName("my-secret-1");
-  const secret2 = kplus.Secret.fromSecretName("my-secret-2");
+  const secret1 = kplus.Secret.fromSecretName('my-secret-1');
+  const secret2 = kplus.Secret.fromSecretName('my-secret-2');
 
   // WHEN
-  const sa = new kplus.ServiceAccount(chart, "my-service-account", {
-    secrets: [secret1]
+  const sa = new kplus.ServiceAccount(chart, 'my-service-account', {
+    secrets: [secret1],
   });
 
   sa.addSecret(secret2);
 
   // THEN
   const manifest = Testing.synth(chart);
-  expect(manifest[0]?.secrets[0]).toStrictEqual({ name: "my-secret-1" });
-  expect(manifest[0]?.secrets[1]).toStrictEqual({ name: "my-secret-2" });
+  expect(manifest[0]?.secrets[0]).toStrictEqual({ name: 'my-secret-1' });
+  expect(manifest[0]?.secrets[1]).toStrictEqual({ name: 'my-secret-2' });
 });
