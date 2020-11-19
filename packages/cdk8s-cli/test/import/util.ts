c62d0a4c86c6256d93cc8b717b081fd4a16de0be
Phil Prasek <prasek@gmail.com>
2020-11-19 03:41:51 -0800

    feat(cli): import from crds.dev (#378)
    Introduce crds.dev import shortcut: `cdk8s import github:account/repo[@VERSION]`
    
    Example:
    `cdk8s import github:crossplane/crossplane@0.14.0`
    
    Resolves #377
    
    In a future PR would like to generate a single module per k8s API group with all CRDs for that API group, so you could:
    ```ts
    import { CompositeResourceDefinition, Composition } from './imports/apiextensions.crossplane.io';
    ```
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/cdk8s-cli/test/import/util.ts
+++ packages/cdk8s-cli/test/import/util.ts
@@ -3,28 +3,33 @@ import * as path from 'path';
 import { ImportBase, ImportOptions, Language } from '../../src/import/base';
 import { mkdtemp } from '../../src/util';
 
-export function expectImportMatchSnapshot(name: string, fn: () => ImportBase, options?: Partial<ImportOptions>) {
-  jest.setTimeout(3 * 60_000);
+jest.setTimeout(3 * 60_000);
+
+export function testImportMatchSnapshot(name: string, fn: () => ImportBase, options?: Partial<ImportOptions>) {
 
   test(name, async () => {
-    await mkdtemp(async workdir => {
-      const importer = fn();
-      const jsiiPath = path.join(workdir, '.jsii');
+    await expectImportMatchSnapshot(fn, options);
+  });
+}
 
-      await importer.import({
-        outdir: workdir,
-        outputJsii: jsiiPath,
-        targetLanguage: Language.TYPESCRIPT,
-        ...options,
-      });
+export async function expectImportMatchSnapshot(fn: () => ImportBase, options?: Partial<ImportOptions>) {
+  await mkdtemp(async workdir => {
+    const importer = fn();
+    const jsiiPath = path.join(workdir, '.jsii');
 
-      const manifest = JSON.parse((await fs.readFile(jsiiPath)).toString('utf-8'));
-
-      // patch cdk8s version in manifest because it's not stable
-      manifest.dependencies.cdk8s = '999.999.999';
-      manifest.fingerprint = '<fingerprint>';
-
-      expect(manifest).toMatchSnapshot();
+    await importer.import({
+      outdir: workdir,
+      outputJsii: jsiiPath,
+      targetLanguage: Language.TYPESCRIPT,
+      ...options,
     });
+
+    const manifest = JSON.parse((await fs.readFile(jsiiPath)).toString('utf-8'));
+
+    // patch cdk8s version in manifest because it's not stable
+    manifest.dependencies.cdk8s = '999.999.999';
+    manifest.fingerprint = '<fingerprint>';
+
+    expect(manifest).toMatchSnapshot();
   });
 }
