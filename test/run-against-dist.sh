9d8630d7ca3bdde5510e9d64c5b9fe22ba6aa529
campionfellin <campionfellin@gmail.com>
2020-12-03 18:34:08 -0800

    add quotes
    Signed-off-by: campionfellin <campionfellin@gmail.com>

--- test/run-against-dist.sh
+++ test/run-against-dist.sh
@@ -22,8 +22,8 @@ cd ${staging}
 npm init -y > /dev/null
 npm install ${CDK8S_DIST}/js/*.tgz
 export PATH=${staging}/node_modules/.bin:$PATH
+export PATH="${staging}/node_modules/.bin:$PATH"
 
-icacls ${staging}/node_modules/.bin/cdk8s
 # restore working directory
 cd $cwd
 $@
