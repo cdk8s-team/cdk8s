bde148395ad3525ddf12f7a450f743037edce8bc
Eli Polonsky <epolon@amazon.com>
2020-12-05 23:12:13 +0200

    mid work

--- .projenrc/.projenrc.ts
+++ .projenrc/.projenrc.ts
@@ -12,6 +12,22 @@ const project = new pjcontrib.YarnMonoRepo({
   authorOrganization: true,
   authorUrl: 'https://aws.amazon.com',
   stability: 'experimental',
+
+  // the default workflows aren't currently
+  // comptabile with this repo since we need extra tasks,
+  // not just build.
+  // TODO: projen should expose these workflows for customizations.
+  buildWorkflow: false,
+  releaseWorkflow: false,
+
+  // handled by github actions instead
+  dependabot: false,
+
+  // since the workflows are disabled, the default mergify configuration
+  // does not include a build success criteria.
+  // TODO: projen should expose this for customizations.
+  mergify: false,
+
   keywords: [
     "cdk",
     "kubernetes",
@@ -52,7 +68,7 @@ const pack = project.tasks.addTask('package');
 pack.exec('tools/pack.sh');
 pack.exec('tools/collect-dist.sh');
 
-project.tasks.addTask('align-version', { exec: 'tools/align-version.sh' });
+const alignVersion = project.tasks.addTask('align-version', { exec: 'tools/align-version.sh' });
 
 const nuke = project.tasks.addTask('nuke', { exec: 'rm -rf **/node_modules **/dist' });
 const rebuild = project.tasks.addTask('rebuild');
@@ -60,9 +76,16 @@ const rebuild = project.tasks.addTask('rebuild');
 rebuild.spawn(nuke);
 rebuild.spawn(project.buildTask);
 
-project.tasks.addTask('integ', { exec: 'test/run-against-dist test/test-all.sh' });
+const integ = project.tasks.addTask('integ', { exec: 'test/run-against-dist test/test-all.sh' });
 project.tasks.addTask('integ:update', { exec: 'UPDATE_SNAPSHOTS=1 yarn integ' });
 project.tasks.addTask('release-github', { exec: 'tools/release-github.sh' });
+
+const ci = project.tasks.addTask('ci');
+ci.spawn(alignVersion);
+ci.spawn(project.compileTask);
+ci.spawn(project.testTask);
+ci.spawn(pack);
+ci.spawn(integ);
 
 const constructs = '3.2.56';
 
