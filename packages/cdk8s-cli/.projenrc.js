d6248fc9663f3d077d52937d558726ef2aeb7a31
Eli Polonsky <Eli.polonsky@gmail.com>
2020-12-16 16:16:47 +0200

    chore(deps): auto dependencies upgrade (#457)

--- packages/cdk8s-cli/.projenrc.js
+++ packages/cdk8s-cli/.projenrc.js
@@ -6,12 +6,12 @@ const project = new TypeScriptLibraryProject({
   name: 'cdk8s-cli',
   description: 'CDK for Kubernetes CLI',
   bin: {
-    cdk8s: 'bin/cdk8s'
+    cdk8s: 'bin/cdk8s',
   },
   deps: [
-    'cdk8s@0.0.0',
+    'cdk8s@^0.0.0',
     'codemaker',
-    `constructs@^${common.versions.constructs}`,
+    `constructs`,
     'fs-extra',
     'jsii-srcmak',
     'jsii-pacmak',
@@ -19,19 +19,17 @@ const project = new TypeScriptLibraryProject({
     'yaml',
     'yargs',
     'json2jsii',
-    'colors'
+    'colors',
+
+    // add @types/node as a regular dependency since it's needed to during "import"
+    // to compile the generated jsii code.
+    '@types/node',
   ],
   devDeps: [
     '@types/fs-extra',
     '@types/json-schema',
   ],
   ...common.options,
-});
-
-// add @types/node as a regular dependency since it's needed to during "import"
-// to compile the generated jsii code.
-project.addDependencies({
-  '@types/node': Semver.caret(project.devDependencies['@types/node'].replace(/^\^/, ''))
 });
 
 project.eslint.addIgnorePattern('/templates/');
--- packages/cdk8s-plus-17/.projenrc.js
+++ packages/cdk8s-plus-17/.projenrc.js
@@ -1,6 +1,6 @@
+const path = require('path');
 const { JsiiProject, Semver } = require('projen');
 const common = require('../projen-common');
-const path = require('path');
 
 const SPEC_VERSION = '17';
 
@@ -9,38 +9,42 @@ const project = new JsiiProject({
   description: 'High level abstractions on top of cdk8s',
   stability: common.options.stability,
 
+  // without this, the version of 'constructs' would need to be controlled
+  // from this file, since otherwise it would create a 0.0.0 dev dependency.
+  peerDependencyOptions: {
+    pinnedDevDependency: false,
+  },
   ...common.options,
 
-  // dependencies
-  jsiiVersion: Semver.caret(common.versions.jsii),
-  peerDependencies: {
-    "cdk8s": Semver.caret('0.0.0'),
-    "constructs": Semver.caret(common.versions.constructs),
-  },
-  dependencies: {
-    minimatch: Semver.caret('3.0.4'),
-  },
-  bundledDependencies: [ 'minimatch' ],
-  devDependencies: {
-    '@types/minimatch': Semver.caret('3.0.3'),
-    "cdk8s": Semver.caret('0.0.0'),
-    "constructs": Semver.pinned(common.versions.constructs),
-  },
+  peerDeps: [
+    'cdk8s@^0.0.0',
+    'constructs',
+  ],
+  deps: [
+    'minimatch',
+  ],
+  bundledDeps: ['minimatch'],
+  devDeps: [
+    'constructs',
+    '@types/minimatch',
+    'cdk8s@0.0.0',
+    'constructs',
+  ],
 
   // jsii configuration
   java: {
     javaPackage: `org.cdk8s.plus${SPEC_VERSION}`,
     mavenGroupId: 'org.cdk8s',
-    mavenArtifactId: `cdk8s-plus-${SPEC_VERSION}`
+    mavenArtifactId: `cdk8s-plus-${SPEC_VERSION}`,
   },
   python: {
     distName: `cdk8s-plus-${SPEC_VERSION}`,
-    module: `cdk8s_plus_${SPEC_VERSION}`
+    module: `cdk8s_plus_${SPEC_VERSION}`,
   },
   dotnet: {
     dotNetNamespace: `Org.Cdk8s.Plus${SPEC_VERSION}`,
-    packageId: `Org.Cdk8s.Plus${SPEC_VERSION}`
-  }
+    packageId: `Org.Cdk8s.Plus${SPEC_VERSION}`,
+  },
 });
 
 common.fixup(project);
@@ -48,5 +52,5 @@ common.fixup(project);
 const K8S_VERSION = '1.17.0';
 const importdir = path.join('src', 'imports');
 
-project.addScript('import', `../cdk8s-cli/bin/cdk8s -l typescript -o ${importdir} import k8s@${K8S_VERSION}`);
+project.setScript('import', `../cdk8s-cli/bin/cdk8s -l typescript -o ${importdir} import k8s@${K8S_VERSION}`);
 project.synth();
