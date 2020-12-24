065756ef89afa938cc120f3af83b23851e06098d
Elad Ben-Israel <benisrae@amazon.com>
2020-12-24 13:29:24 +0200

    feat(cli): new version notifications (#454)
    The CLI will print a notification if there is a new version of the CDK8s CLI available in npm.
    
    The result is cached under ~/.cdk8s-cli.version with a TTL of 30min.
    
    Resolves #452
    
    ---
    
    <img width="963" alt="Screen Shot 2020-12-15 at 23 06 38" src="https://user-images.githubusercontent.com/598796/102272933-3a0c6800-3f2a-11eb-9fc0-60a10cb341bb.png">
    
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/cdk8s-cli/src/cli/index.ts
+++ packages/cdk8s-cli/src/cli/index.ts
@@ -1,15 +1,33 @@
+import * as colors from 'colors';
 import * as yargs from 'yargs';
+import { upgradeAvailable } from '../upgrades';
 
-const args = yargs
-  .commandDir('cmds')
-  .recommendCommands()
-  .wrap(yargs.terminalWidth())
-  .showHelpOnFail(false)
-  .env('CDK8S')
-  .epilogue('Options can be specified via environment variables with the "CDK8S_" prefix (e.g. "CDK8S_OUTPUT")')
-  .help()
-  .argv;
+async function main() {
+  const versions = upgradeAvailable();
+  if (versions) {
+    console.error('------------------------------------------------------------------------------------------------');
+    console.error(colors.yellow(`A new version ${versions.latest} of cdk8s-cli is available (current ${versions.current}).`));
+    console.error(colors.yellow('Run "npm install -g cdk8s-cli" to install the latest version on your system.'));
+    console.error(colors.yellow('For additional installation methods, see https://cdk8s.io/docs/latest/getting-started'));
+    console.error('------------------------------------------------------------------------------------------------');
+  }
 
-if (args._.length === 0) {
-  yargs.showHelp();
+  const ya = yargs
+    .commandDir('cmds')
+    .recommendCommands()
+    .wrap(yargs.terminalWidth())
+    .showHelpOnFail(false)
+    .env('CDK8S')
+    .epilogue('Options can be specified via environment variables with the "CDK8S_" prefix (e.g. "CDK8S_OUTPUT")')
+    .help();
+
+  const args = ya.argv;
+  if (args._.length === 0) {
+    yargs.showHelp();
+  }
 }
+
+main().catch(e => {
+  console.error(e.stack);
+  process.exit(1);
+});
