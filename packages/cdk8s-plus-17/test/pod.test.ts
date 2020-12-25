5cf2fa1c59ec3520f7d19fdf90ff7ea07c4b785a
Eli Polonsky <epolon@amazon.com>
2020-12-25 15:19:42 +0200

    added test

--- packages/cdk8s-plus-17/test/pod.test.ts
+++ packages/cdk8s-plus-17/test/pod.test.ts
@@ -2,6 +2,50 @@ import { Testing, ApiObject } from 'cdk8s';
 import { Node } from 'constructs';
 import * as kplus from '../src';
 
+test('multiple mounts per volume', () => {
+
+  const chart = Testing.chart();
+
+  const configMap = new kplus.ConfigMap(chart, 'Config', {
+    data: {
+      f1: 'f1-content',
+      f2: 'f2-content',
+    },
+  });
+
+  const volume = kplus.Volume.fromConfigMap(configMap);
+
+  new kplus.Pod(chart, 'Pod', {
+    containers: [
+      {
+        image: 'image',
+        volumeMounts: [
+          {
+            volume: volume,
+            path: 'f1',
+            subPath: 'f1',
+          },
+          {
+            volume: volume,
+            path: 'f2',
+            subPath: 'f2',
+          },
+        ],
+      },
+    ],
+  });
+
+  const podSpec = Testing.synth(chart).filter(r => r.kind === 'Pod')[0].spec;
+
+  expect(podSpec.volumes).toEqual([{
+    configMap: {
+      name: 'test-config-c8c927dd',
+    },
+    name: 'configmap-test-config-c8c927dd',
+  }]);
+
+});
+
 test('defaultChild', () => {
 
   const chart = Testing.chart();
