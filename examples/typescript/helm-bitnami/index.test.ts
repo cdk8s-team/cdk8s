10ec6a1700136a7e253ec0926f223bfe91a1976b
campionfellin <campionfellin@gmail.com>
2021-02-11 20:51:11 -0800

    remove typescript test because it cannot download helm chart
    Signed-off-by: campionfellin <campionfellin@gmail.com>

--- examples/typescript/helm-bitnami/index.test.ts
+++
@@ -1,8 +0,0 @@
-import { HelloHelm } from './index';
-import { Testing } from 'cdk8s';
-
-test('snapshot', () => {
-  const app = Testing.app();
-  const chart = new HelloHelm(app, 'hello-helm');
-  expect(Testing.synth(chart)).toMatchSnapshot();
-});
