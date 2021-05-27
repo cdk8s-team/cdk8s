8d30495f93614269756cf8f719ed3ce6a9fec743
Elad Ben-Israel <benisrae@amazon.com>
2021-05-27 15:54:08 +0300

    chore: extract modules to separate repos
    Deletes the code of everything under `packages/**` and takes a dependency on the published modules instead.

--- packages/cdk8s-cli/test/synth/synth-stdout.test.ts
+++
@@ -1,7 +0,0 @@
-// eslint-disable-next-line
-const cmd = require('../../src/cli/cmds/synth');
-
-test('synth with both --stdout and --output throws exception', () => {
-  // eslint-disable-next-line
-  expect(cmd.handler({ app: 'cdk8s', output: 'test', stdout: true })).rejects.toEqual(new Error('\'--output\' and \'--stdout\' are mutually exclusive. Please only use one.'));
-});
