1457d05613fb6c820e4f9a2ef5da2b67d7fe34ce
Elad Ben-Israel <benisrae@amazon.com>
2021-06-01 14:16:51 +0300

    Update .projenrc.js

--- .projenrc.js
+++ .projenrc.js
@@ -9,6 +9,7 @@ const project = new NodeProject({
   pullRequestTemplateContents: [
     '*By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*'
   ],
+  projenUpgradeSecret: 'PROJEN_GITHUB_TOKEN',
   releaseWorkflow: false,
   workflowBootstrapSteps: [
     {
