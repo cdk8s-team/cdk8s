93ceafec3a88cf88ad22e708ae9f254c0a2b1dec
Sumu <sumughan@amazon.com>
2023-08-22 15:44:02 -0400

    try removing outputs assignment
    Signed-off-by: Sumu <sumughan@amazon.com>

--- src/k8s-automation.ts
+++ src/k8s-automation.ts
@@ -35,12 +35,12 @@ export class K8sVersionUpgradeAutomation extends Component {
         contents: workflows.JobPermission.READ,
         pullRequests: workflows.JobPermission.WRITE,
       },
-      outputs: {
-        latestVersion: {
-          stepId: 'k8s-latest-version',
-          outputName: 'latestVersion',
-        },
-      },
+      //   outputs: {
+      //     latestVersion: {
+      //       stepId: 'k8s-latest-version',
+      //       outputName: 'latestVersion',
+      //     },
+      //   },
       steps: [
         {
           id: 'get-k8s-latest-release',
@@ -54,14 +54,14 @@ export class K8sVersionUpgradeAutomation extends Component {
         {
           id: 'k8s-latest-version',
           name: 'Get latest K8s minor version',
-          run: 'echo latestVersion="$(cut -d "." -f 2 <<< "${{ steps.get-k8s-latest-release.outputs.release }}")"',
-        //   run: 'echo "$(cut -d "." -f 2 <<< "${{ steps.get-k8s-latest-release.outputs.release }}")"',
+          //   run: 'echo latestVersion="$(cut -d "." -f 2 <<< "${{ steps.get-k8s-latest-release.outputs.release }}")"',
+          run: 'echo "$(cut -d "." -f 2 <<< "${{ steps.get-k8s-latest-release.outputs.release }}")"',
         },
         {
           id: 'check-if-cdk8s-is-updated',
           name: 'Check to see if cdk8s-plus released latest k8s version on npm',
           // returns 1 if website is valid, 0 if invalid
-          run: 'wget -q  -O /tmp/foo https://www.npmjs.com/package/cdk8s-plus-${{check-latest-k8s-release.outputs.latestVersion}} | grep "200" /tmp/foo | wc -l',
+          run: 'wget -q  -O /tmp/foo https://www.npmjs.com/package/cdk8s-plus-${{steps.k8s-latest-version}} | grep "200" /tmp/foo | wc -l',
         },
       ],
     };
