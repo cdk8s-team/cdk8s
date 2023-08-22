8b87c6e768e290b765d73aa32e8bdde0c9cbc016
Sumu <sumughan@amazon.com>
2023-08-22 14:35:16 -0400

    try fixing bash command
    Signed-off-by: Sumu <sumughan@amazon.com>

--- src/k8s-automation.ts
+++ src/k8s-automation.ts
@@ -51,7 +51,7 @@ export class K8sVersionUpgradeAutomation extends Component {
         {
           id: 'k8s-latest-version',
           name: 'Get latest K8s minor version',
-          run: 'echo "${{ steps.get-k8s-latest-release.outputs.release}}: 2"',
+          run: 'echo "{${{ steps.get-k8s-latest-release.outputs.release}}}: 2"',
         //   uses: 'pozetroninc/github-action-get-latest-release@master',
         //   with: {
         //     repository: 'kubernetes/kubernetes',
