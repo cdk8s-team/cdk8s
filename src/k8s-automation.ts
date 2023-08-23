fee34d1bb33d52a1f7306b8edbfec130b708db99
Sumu <sumughan@amazon.com>
2023-08-23 12:25:16 -0400

    add first job to needs list
    Signed-off-by: Sumu <sumughan@amazon.com>

--- src/k8s-automation.ts
+++ src/k8s-automation.ts
@@ -75,6 +75,7 @@ export class K8sVersionUpgradeAutomation extends Component {
         contents: workflows.JobPermission.READ,
         pullRequests: workflows.JobPermission.WRITE,
       },
+      needs: ['check-latest-k8s-release'],
       if: '${{ needs.check-latest-k8s-release.steps.get-npm-status-code.outputs.httpStatu }} == "200"',
       steps: [
         {
