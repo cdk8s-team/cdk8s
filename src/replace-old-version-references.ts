8d552bdd9ebab365636b425a3c60729d0076d52d
Sumu <sumughan@amazon.com>
2023-08-24 12:11:23 -0400

    fix eslint spacing errors in find replace ts file
    Signed-off-by: Sumu <sumughan@amazon.com>

--- src/replace-old-version-references.ts
+++ src/replace-old-version-references.ts
@@ -3,39 +3,39 @@ import * as path from 'path';
 
 function main(latestVersion: string) {
 
-    const latestVersionNumber = Number(latestVersion);
+  const latestVersionNumber = Number(latestVersion);
 
-    const filesToBeUpdated = ['.projenrc.js', 'docs/build.sh', 'website/build.sh', 'website/layouts/index.html', 'docs/plus/.pages', 'docs/references/.pages', 'docs/reference/index.md', ];
-    
-    filesToBeUpdated.forEach(function (filePath) {
-        let curFileData = fs.readFileSync(filePath, 'utf-8');
-        for (let i = 0; i < 3; i++) {
-            curFileData = curFileData.replace(`${latestVersionNumber - 1 - i}`, `${latestVersionNumber - i}`);
-        }
-        fs.writeFileSync(filePath, curFileData);
-    });
+  const filesToBeUpdated = ['.projenrc.js', 'docs/build.sh', 'website/build.sh', 'website/layouts/index.html', 'docs/plus/.pages', 'docs/references/.pages', 'docs/reference/index.md', ];
 
-    // make new docs/reference/cdk8s-plus-XX/ directory and add .pages and go.md files
-    const newDocsReferenceDir = `../docs/reference/cdk8s-plus-${latestVersionNumber}/`;
-    fs.mkdirSync(newDocsReferenceDir);
-    const pagesFileData = fs.readFileSync(`../docs/reference/cdk8s-plus-${latestVersionNumber-1}/.pages`, 'utf-8');
-    fs.writeFileSync(path.join(newDocsReferenceDir, '.pages'), pagesFileData);
-    let goFileData = fs.readFileSync(`../docs/reference/cdk8s-plus-${latestVersionNumber-1}/go.md`, 'utf-8');
-    goFileData = goFileData.replace(`${latestVersionNumber - 1}`, `${latestVersionNumber}`);
-    fs.writeFileSync(path.join(newDocsReferenceDir, 'go.md'), goFileData);
+  filesToBeUpdated.forEach(function (filePath) {
+    let curFileData = fs.readFileSync(filePath, 'utf-8');
+    for (let i = 0; i < 3; i++) {
+      curFileData = curFileData.replace(`${latestVersionNumber - 1 - i}`, `${latestVersionNumber - i}`);
+    }
+    fs.writeFileSync(filePath, curFileData);
+  });
 
-    // update reference in docs/plus/index.md
-    let plusIndexFileData = fs.readFileSync('docs/plus/index.md', 'utf-8');
-    plusIndexFileData = plusIndexFileData.replace(`${latestVersionNumber - 1}`, `${latestVersionNumber}`);
-    fs.writeFileSync('../docs/plus/index.md', plusIndexFileData);
+  // make new docs/reference/cdk8s-plus-XX/ directory and add .pages and go.md files
+  const newDocsReferenceDir = `../docs/reference/cdk8s-plus-${latestVersionNumber}/`;
+  fs.mkdirSync(newDocsReferenceDir);
+  const pagesFileData = fs.readFileSync(`../docs/reference/cdk8s-plus-${latestVersionNumber-1}/.pages`, 'utf-8');
+  fs.writeFileSync(path.join(newDocsReferenceDir, '.pages'), pagesFileData);
+  let goFileData = fs.readFileSync(`../docs/reference/cdk8s-plus-${latestVersionNumber-1}/go.md`, 'utf-8');
+  goFileData = goFileData.replace(`${latestVersionNumber - 1}`, `${latestVersionNumber}`);
+  fs.writeFileSync(path.join(newDocsReferenceDir, 'go.md'), goFileData);
 
-    // update all references in docs/basics/**
-    const docsFileNames = fs.readdirSync('docs/basics/', { encoding: 'utf-8'});
-    docsFileNames.forEach(function (filePath) {
-        let curFileData = fs.readFileSync(path.join('docs/basics', filePath), 'utf-8');
-        curFileData = curFileData.replace(`${latestVersionNumber - 1}`, `${latestVersionNumber}`);
-        fs.writeFileSync(path.join('docs/basics', filePath), curFileData);
-    });
+  // update reference in docs/plus/index.md
+  let plusIndexFileData = fs.readFileSync('docs/plus/index.md', 'utf-8');
+  plusIndexFileData = plusIndexFileData.replace(`${latestVersionNumber - 1}`, `${latestVersionNumber}`);
+  fs.writeFileSync('../docs/plus/index.md', plusIndexFileData);
+
+  // update all references in docs/basics/**
+  const docsFileNames = fs.readdirSync('docs/basics/', { encoding: 'utf-8' });
+  docsFileNames.forEach(function (filePath) {
+    let curFileData = fs.readFileSync(path.join('docs/basics', filePath), 'utf-8');
+    curFileData = curFileData.replace(`${latestVersionNumber - 1}`, `${latestVersionNumber}`);
+    fs.writeFileSync(path.join('docs/basics', filePath), curFileData);
+  });
 }
 
 main(process.argv.slice(2)[0]);
