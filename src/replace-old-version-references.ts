14a4dfc3886205cbae487ff38d15db0471749937
Sumu <sumughan@amazon.com>
2023-08-24 12:24:03 -0400

    remove ../ from docs file paths
    Signed-off-by: Sumu <sumughan@amazon.com>

--- src/replace-old-version-references.ts
+++ src/replace-old-version-references.ts
@@ -16,18 +16,18 @@ function main(latestVersion: string) {
   });
 
   // make new docs/reference/cdk8s-plus-XX/ directory and add .pages and go.md files
-  const newDocsReferenceDir = `../docs/reference/cdk8s-plus-${latestVersionNumber}/`;
+  const newDocsReferenceDir = `docs/reference/cdk8s-plus-${latestVersionNumber}/`;
   fs.mkdirSync(newDocsReferenceDir);
-  const pagesFileData = fs.readFileSync(`../docs/reference/cdk8s-plus-${latestVersionNumber-1}/.pages`, 'utf-8');
+  const pagesFileData = fs.readFileSync(`docs/reference/cdk8s-plus-${latestVersionNumber-1}/.pages`, 'utf-8');
   fs.writeFileSync(path.join(newDocsReferenceDir, '.pages'), pagesFileData);
-  let goFileData = fs.readFileSync(`../docs/reference/cdk8s-plus-${latestVersionNumber-1}/go.md`, 'utf-8');
+  let goFileData = fs.readFileSync(`docs/reference/cdk8s-plus-${latestVersionNumber-1}/go.md`, 'utf-8');
   goFileData = goFileData.replace(`${latestVersionNumber - 1}`, `${latestVersionNumber}`);
   fs.writeFileSync(path.join(newDocsReferenceDir, 'go.md'), goFileData);
 
   // update reference in docs/plus/index.md
   let plusIndexFileData = fs.readFileSync('docs/plus/index.md', 'utf-8');
   plusIndexFileData = plusIndexFileData.replace(`${latestVersionNumber - 1}`, `${latestVersionNumber}`);
-  fs.writeFileSync('../docs/plus/index.md', plusIndexFileData);
+  fs.writeFileSync('docs/plus/index.md', plusIndexFileData);
 
   // update all references in docs/basics/**
   const docsFileNames = fs.readdirSync('docs/basics/', { encoding: 'utf-8' });
