0d4f2440f4c769441349aeb2334668b13e9f2b2f
Sumu <sumughan@amazon.com>
2023-08-25 13:52:58 -0400

    add function to only find and replace references with certain plus prefixes, so it doesn't find and replace numbers where it shouldn't
    Signed-off-by: Sumu <sumughan@amazon.com>

--- src/replace-old-version-references.ts
+++ src/replace-old-version-references.ts
@@ -1,6 +1,20 @@
 import * as fs from 'fs';
 import * as path from 'path';
 
+function replaceRefsInFile(filePath: string, toReplace: string, substitution: string) {
+  // references in the docs files for version XX appear in the following ways:
+  // cdk8s-plus-XX, cdk8s.plusXX, cdk8splusXX, CDK8s_PLUSXX_VERSION, cdk8s_plus28_version
+  // we must also replace references to outdated kubernetes versions. this shows up as:
+  // Kubernetes v1.XX.0
+  const referencePrefixes = ['plus-', 'plus', 'PLUS', 'Kubernetes v1.'];
+
+  referencePrefixes.forEach(function (referencePrefix: string) {
+    let curFileData = fs.readFileSync(filePath, 'utf-8');
+    curFileData = curFileData.replace(new RegExp(referencePrefix + toReplace, 'g'), substitution);
+    fs.writeFileSync(filePath, curFileData);
+  });
+}
+
 function main(latestVersion: string) {
 
   const latestVersionNumber = Number(latestVersion);
@@ -8,14 +22,10 @@ function main(latestVersion: string) {
   const filesToBeUpdated = ['.projenrc.ts', 'docs/build.sh', 'website/build.sh', 'website/layouts/index.html', 'docs/plus/.pages', 'docs/reference/.pages', 'docs/reference/index.md'];
 
   filesToBeUpdated.forEach(function (filePath: string) {
-    let curFileData = fs.readFileSync(filePath, 'utf-8');
     for (let i = 0; i < 3; i++) {
-      curFileData = curFileData.replace(new RegExp(`${latestVersionNumber - 1 - i}`, 'g'), `${latestVersionNumber - i}`);
+      replaceRefsInFile(filePath, `${latestVersionNumber - 1 - i}`, `${latestVersionNumber - i}`);
     }
-    fs.writeFileSync(filePath, curFileData);
   });
-
-  const replacementRegex = new RegExp(`${latestVersionNumber - 1}`, 'g');
 
   // make new docs/reference/cdk8s-plus-XX/ directory and add .pages and go.md files
   const newDocsReferenceDir = `docs/reference/cdk8s-plus-${latestVersionNumber}/`;
@@ -23,20 +33,16 @@ function main(latestVersion: string) {
   const pagesFileData = fs.readFileSync(`docs/reference/cdk8s-plus-${latestVersionNumber-1}/.pages`, 'utf-8');
   fs.writeFileSync(path.join(newDocsReferenceDir, '.pages'), pagesFileData);
   let goFileData = fs.readFileSync(`docs/reference/cdk8s-plus-${latestVersionNumber-1}/go.md`, 'utf-8');
-  goFileData = goFileData.replace(replacementRegex, `${latestVersionNumber}`);
   fs.writeFileSync(path.join(newDocsReferenceDir, 'go.md'), goFileData);
+  replaceRefsInFile(path.join(newDocsReferenceDir, 'go.md'), `${latestVersionNumber - 1}`, latestVersion);
 
   // update reference in docs/plus/index.md
-  let plusIndexFileData = fs.readFileSync('docs/plus/index.md', 'utf-8');
-  plusIndexFileData = plusIndexFileData.replace(replacementRegex, `${latestVersionNumber}`);
-  fs.writeFileSync('docs/plus/index.md', plusIndexFileData);
+  replaceRefsInFile('docs/plus/index.md', `${latestVersionNumber - 1}`, latestVersion);
 
   // update all references in docs/basics/**
   const docsFileNames = fs.readdirSync('docs/basics/', { encoding: 'utf-8' });
   docsFileNames.forEach(function (filePath) {
-    let curFileData = fs.readFileSync(path.join('docs/basics', filePath), 'utf-8');
-    curFileData = curFileData.replace(replacementRegex, `${latestVersionNumber}`);
-    fs.writeFileSync(path.join('docs/basics', filePath), curFileData);
+    replaceRefsInFile(filePath, `${latestVersionNumber - 1}`, latestVersion);
   });
 }
 
