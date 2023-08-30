dd613e59c13b3e0c4dec65fd25a430ec4d7745b6
Eli Polonsky <epolon@amazon.com>
2023-08-30 17:43:49 +0300

    chore: remove cloud tokens example (#1465)
    It was accidentally merged along with the RFC, but it doesn't actually belong here. It also causes this security [alert](https://github.com/cdk8s-team/cdk8s/security/dependabot/26).
    
    Closes https://github.com/cdk8s-team/cdk8s/issues/1454

--- examples/typescript/resolve-cloud-tokens/apps/awscdk-app.ts
+++
@@ -1,5 +0,0 @@
-import * as awscdk from 'aws-cdk-lib';
-import { AWSCDKStack } from '../constructs/awscdk-stack';
-
-export const awscdkApp = new awscdk.App({ outdir: 'cdk.out' });
-export const awscdkStack = new AWSCDKStack(awscdkApp, 'aws');
--- examples/typescript/resolve-cloud-tokens/apps/cdktf-app.ts
+++
@@ -1,6 +0,0 @@
-import * as cdktf from 'cdktf';
-import { CDKTFStack } from '../constructs/cdktf-stack';
-
-export const cdktfApp = new cdktf.App({ outdir: 'cdktf.out' });
-export const cdktfStack = new CDKTFStack(cdktfApp, 'aws');
-
