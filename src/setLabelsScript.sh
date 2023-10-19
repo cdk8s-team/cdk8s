6b13485b712b08d803ab42c4ea0a8f6e7956ecb0
Sumu <sumughan@amazon.com>
2023-10-19 17:01:34 -0400

    i solved the stupid error ....
    Signed-off-by: Sumu <sumughan@amazon.com>

--- src/setLabelsScript.sh
+++ src/setLabelsScript.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 TESTING_MODE_INPUT="$1"
-if [ TESTING_MODE_INPUT = "true" ]
+if [ $TESTING_MODE_INPUT = "true" ]
 then
   echo labels="auto-approve" >> $GITHUB_OUTPUT
 else
-  echo "SUMU WHY"
-  echo $TESTING_MODE_INPUT
-  # echo labels="" >> $GITHUB_OUTPUT
+  echo labels="" >> $GITHUB_OUTPUT
 fi
