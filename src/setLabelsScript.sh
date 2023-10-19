71bfcceea78ed24e11794398ad0f9e9875a80ce5
Sumu <sumughan@amazon.com>
2023-10-19 16:47:50 -0400

    move then
    Signed-off-by: Sumu <sumughan@amazon.com>

--- src/setLabelsScript.sh
+++ src/setLabelsScript.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 TESTING_MODE_INPUT="$1"
-if [ TESTING_MODE_INPUT = "true" ] then
+if [ TESTING_MODE_INPUT = "true" ]
+then
   echo labels="auto-approve" >> $GITHUB_OUTPUT
 else
   echo labels="" >> $GITHUB_OUTPUT
