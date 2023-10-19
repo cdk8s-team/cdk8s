1c702f4da592b681c708679d5490701e1ff6222b
Sumu <sumughan@amazon.com>
2023-10-19 15:45:00 -0400

    fix script
    Signed-off-by: Sumu <sumughan@amazon.com>

--- src/testModeSet.sh
+++ src/testModeSet.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 TESTING_MODE_INPUT="$1"
-echo ${TESTING_MODE_INPUT:-"true"} >> $GITHUB_ENV
+echo testingMode=${TESTING_MODE_INPUT:-"true"} >> $GITHUB_ENV
 # echo testingMode=${TESTING_MODE_INPUT:-"true"}
