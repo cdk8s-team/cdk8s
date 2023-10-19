fcef4344013e99266c102790e3b94a0a2eb5b134
Sumu <sumughan@amazon.com>
2023-10-19 11:52:43 -0400

    try out separate script file
    Signed-off-by: Sumu <sumughan@amazon.com>

---
+++ src/testingModeScript.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+export testingMode="$(${{github.event.inputs.testingMode}})"
+
+if ["$testingMode" = "false"]; then
+  echo labels="auto-approve" >> $GITHUB_OUTPUT
+fi
+
+# if [-z "$testingMode"]; then
+#   echo labels="auto-approve" >> $GITHUB_OUTPUT
+# fi
+# export isPush="$(get_version cdk8s-plus-$((${LATEST_K8S_VERSION}-2)))"
+# export CDK8S_PLUSXX_MINUS_1_VERSION="$(get_version cdk8s-plus-$((${LATEST_K8S_VERSION}-1)))"
+# export CDK8S_PLUSXX_VERSION="$(get_version cdk8s-plus-${LATEST_K8S_VERSION})"
+# export CDK8S_CLI_VERSION="$(get_version cdk8s-cli)"
+# hugo --minify $@
