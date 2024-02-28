910c6c65b4fd7378539cca78e6fac6c63a0a0f5a
Eli Polonsky <epolon@amazon.com>
2024-02-28 20:39:44 +0200

    fix(website): cdk8s-plus dropdown shows incorrect spec numbers (#1839)
    The dropdown shows the version of the package instead of the spec number:
    
    <img width="265" alt="Screenshot 2024-02-28 at 20 37 08" src="https://github.com/cdk8s-team/cdk8s/assets/1428812/c56625fc-747e-4402-8bef-080daaf5ff82">

--- website/build.sh
+++ website/build.sh
@@ -12,6 +12,9 @@ export CDK8S_CORE_VERSION="$(get_version cdk8s)"
 export CDK8S_PLUSXX_MINUS_2_VERSION="$(get_version cdk8s-plus-$((${LATEST_K8S_VERSION}-2)))"
 export CDK8S_PLUSXX_MINUS_1_VERSION="$(get_version cdk8s-plus-$((${LATEST_K8S_VERSION}-1)))"
 export CDK8S_PLUSXX_VERSION="$(get_version cdk8s-plus-${LATEST_K8S_VERSION})"
+export CDK8S_PLUSXX_MINUS_2="$((${LATEST_K8S_VERSION}-2))"
+export CDK8S_PLUSXX_MINUS_1="$((${LATEST_K8S_VERSION}-1))"
+export CDK8S_PLUSXX="${LATEST_K8S_VERSION}"
 export CDK8S_CLI_VERSION="$(get_version cdk8s-cli)"
 
 hugo --minify $@
